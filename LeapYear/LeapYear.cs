a9667f3074b781a00a8f9224cb145765915011e4
DF Lorenzo <three@hey.com>
2021-11-30 14:03:38 +0100

    feat: controlled result when year divisible by 4 but not by 100

--- LeapYear/LeapYear.cs
+++ LeapYear/LeapYear.cs
@@ -4,6 +4,10 @@
     {
         public static bool IsLeap(int year)
         {
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                return true;
+            }
             return year % 400 == 0;
         }
     }
